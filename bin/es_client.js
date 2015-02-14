9428800d3dc501f6241b693512646b33c51556af
Kishore Yekkanti <kishoreyekkanti@gmail.com>
2015-02-14 12:59:27 +0530

    running on forever

--- bin/es_client.js
+++ bin/es_client.js
@@ -1,6 +1,7 @@
 var elasticsearch = require('elasticsearch');
 var Promise = require('bluebird');
 getEsClient = function(){
+    console.log(process.env.NODE_ENV);
     var host = process.env.NODE_ENV === "production"? "https://5bkexnn4:gzn0t5dus6y7js6q@smoke-5691776.us-east-1.bonsai.io": 'localhost:9200';
     return new elasticsearch.Client({
         host: host
