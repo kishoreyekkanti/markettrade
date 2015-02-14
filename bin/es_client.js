3c49e09c27f0b35553dca793a494c307ab01cf4e
Kishore Yekkanti <kishoreyekkanti@gmail.com>
2015-02-14 14:21:15 +0530

    Minro change in bonsai url

--- bin/es_client.js
+++ bin/es_client.js
@@ -4,7 +4,7 @@ var app_logger = require("../logger/logger");
 getEsClient = function(){
     app_logger.info("Trying to fetch es client for "+process.env.NODE_ENV);
     app_logger.info("BONSAI URL "+ENV['BONSAI_URL']+" or "+process.env['BONSAI_URL']);
-    var host = process.env.NODE_ENV === "production"? ENV['BONSAI_URL']: 'localhost:9200';
+    var host = process.env.NODE_ENV === "production"? process.env.BONSAI_URL: 'localhost:9200';
     return new elasticsearch.Client({
         host: host
     });
