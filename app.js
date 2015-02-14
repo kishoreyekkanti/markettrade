26938de01fbb2f3f4902dbabf18749e2abe7c04c
Kishore Yekkanti <kishoreyekkanti@gmail.com>
2015-02-14 13:28:15 +0530

    added heroku dynamic port

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
 });
 
 var server = http.createServer(app);
-server.listen(app.get('port'), function () {
+server.listen(process.env.PORT || app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
--- routes/trades.js
+++ routes/trades.js
@@ -1,13 +1,15 @@
 var ROOT_DIR = process.env.PWD;
 var express = require('express');
 var router = express.Router();
-var tradeController = require(ROOT_DIR + '/controllers/trade_controller');
+var tradeController = require('../controllers/trade_controller');
+var app_logger = require("../logger/logger");
 
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
 router.post('/', function(req, res, next){
+    app_logger.info("Got request to save a trade");
     tradeController.save(req, res);
 });
 module.exports = router;
